f94628e4674afcfbccf428285d013b10d466c63b
Mohammed Amine AYACHE <maayache@wundermancommerce.com>
2022-11-28 21:23:24 +0100

    search

--- src/app/views/pages/buyer/buyer/suppliers/suppliers/suppliers.component.ts
+++ src/app/views/pages/buyer/buyer/suppliers/suppliers/suppliers.component.ts
@@ -30,6 +30,7 @@ export class SuppliersComponent implements OnInit {
   doingActionTo:number=null;
   currentPage:number=0;
   currentSize:number=10;
+  search=null;
 
   spec:Specification=new Specification();
 
@@ -39,6 +40,7 @@ export class SuppliersComponent implements OnInit {
   selectedFamily:string[]=[];
 
   socials:string[]=[];
+  socialsGlobal:string[]=[];
   family:ValueLabelModel[]=[];
   functions:ValueLabelModel[]=[];
   activities:ValueLabelModel[]=[];
@@ -99,6 +101,7 @@ export class SuppliersComponent implements OnInit {
     {
       this.buyerService.getSocialReasons().subscribe(response=>{
         this.socials=response;
+        this.socialsGlobal=response;
       })
       this.generalService.getSupplierConsts().subscribe(response=>{
         this.family=response.filter(e=>e.name=="salesfamily")[0].data;
@@ -168,4 +171,21 @@ export class SuppliersComponent implements OnInit {
         return 0;
       return data.reduce((a,b)=>a+b.rate,0)/data.length;
     }
+
+  onKey(value) { 
+    if(!this.search||this.search.trim()=="")
+    {
+      this.socials=this.socialsGlobal;
+      return;
+    }
+    this.socials=this.socialsGlobal.filter(e=>e.toLowerCase().includes(this.search.toLowerCase()))
+  }
+  openChanged(e)
+  {
+    if(e==false)
+    {
+      this.socials=this.socialsGlobal;
+      this.search=null;
+    }
+  }
 }
