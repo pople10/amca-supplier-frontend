2d48685b2801f134e4ced199a9b8bfdf64f991c0
Mohammed Amine AYACHE <med.amine.ayache@gmail.com>
2023-12-24 22:55:01 +0100

    Naming server

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const ENV = {
-  "backend-api-base-url" : "http://amca.live:8080",
-  "backend-api-websocket-url":"ws://amca.live:8080"
+  "backend-api-base-url" : "http://89.117.36.4:8080",
+  "backend-api-websocket-url":"ws://89.117.36.4:8080"
 };
